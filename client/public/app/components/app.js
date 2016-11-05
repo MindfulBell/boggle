decb0e7282a7c7d7a015ea383829007401229120
MindfulBell <tim.bell02@gmail.com>
2016-11-05 15:33:55 -0700

    webpack and boilerplate for mobx/react

---
+++ client/public/app/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+	// constructor(props) {
+	// 	super(props);
+		
+	// }
+	render() {
+		return <h1> Here is the App </h1>
+	}
+}
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const BUILD_DIR = path.resolve(`${__dirname}/client/public`);
+const APP_DIR = path.resolve(`${__dirname}/client/public/app`);
+
+const config = {
+	entry: `${APP_DIR}/index.js`,
+	output: {
+		path: BUILD_DIR,
+		filename: 'bundle.js',
+		publicPath: '/'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['react', 'es2015', 'react-hmre'],
+					plugins: ['transform-decorators-legacy', 'transform-class-properties']
+				}
+			},
+			{
+				test: /\.scss$/,
+				loaders: ['style', 'css', 'sass']
+			}
+		]
+	},
+	devServer: {
+		historyApiFallback: true,
+		contentBase: path.join('./client/public'),
+		hot: true,
+		inline: true,
+		stats: {
+			colors: true
+		}
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin()
+	]
+}
+
+module.exports = config;
